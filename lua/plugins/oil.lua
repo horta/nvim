00f79299364d8e0c0dd0f07fdfb3324e4d979933
Danilo Horta <danilo.horta@pm.me>
2026-05-03 06:47:36 +0100

    Yank git-relative path under cursor in Oil with yp
    Inside an Oil buffer, yp now copies the entry's path relative to the
    git repo root (or absolute, when the entry isn't tracked). Faster than
    walking up to find a path to share in code review.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,45 @@ return {
   "stevearc/oil.nvim",
   opts = {},
   cmd = "Oil",
-  keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } },
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    -- {
+    --   "yp",
+    --   function()
+    --     local actions = require("oil.actions")
+    --     actions.copy_entry_path.callback()
+    --     vim.fn.setreg("+", vim.fn.getreg(vim.v.register))
+    --   end,
+    --   ft = "oil", -- ensure it only applies inside Oil buffer
+    --   desc = "Copy filepath to system clipboard",
+    -- },
+    {
+      "yp",
+      function()
+        local oil = require("oil")
+        local entry = oil.get_cursor_entry()
+        if not entry then
+          return
+        end
+        local dir = oil.get_current_dir()
+        if not dir then
+          return
+        end
+        local abs_path = vim.fs.normalize(dir .. entry.name)
+
+        -- find git root
+        local git_root = vim.fn.systemlist("git -C " .. vim.fn.shellescape(abs_path) .. " rev-parse --show-toplevel")[1]
+        if git_root and git_root ~= "" then
+          local rel_path = vim.fn.fnamemodify(abs_path, ":." .. git_root)
+          vim.fn.setreg("+", rel_path)
+          print("Yanked relative path: " .. rel_path)
+        else
+          vim.fn.setreg("+", abs_path)
+          print("Yanked absolute path (no git root found): " .. abs_path)
+        end
+      end,
+    },
+  },
   init = function()
     if vim.fn.argc() == 1 then
       local filename = tostring(vim.fn.argv(0))
