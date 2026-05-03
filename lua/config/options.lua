a6c8b29ec72f05a4abc4fa7dc875691ccaaca957
Danilo Horta <danilo.horta@pm.me>
2026-05-03 06:44:27 +0100

    Point python3_host_prog at dedicated ~/.neovim-venv
    Move off the pyenv-managed 3.12.7 interpreter so the Neovim Python host
    isn't tied to whichever pyenv version happens to be installed.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "/Users/horta/.pyenv/versions/3.12.7/bin/python3"
+vim.g.python3_host_prog = "/Users/horta/.neovim-venv/bin/python"
 vim.g.autoformat = false
 vim.o.relativenumber = false
 -- vim.cmd([[autocmd FileType c setlocal colorcolumn=80]])
