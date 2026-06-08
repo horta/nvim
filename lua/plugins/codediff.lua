7866692c74e2a4a7111656f89748bfc491f2987f
Danilo Horta <danilo.horta@pm.me>
2026-06-08 11:25:35 +0100

    feat(plugins): add Diffview PR-review keymap, drop codediff keymap
    Bind <leader>gp to DiffviewOpen origin/main...HEAD for GitHub-style PR
    review (merge-base three-dot diff). Remove the unused <leader>gc CodeDiff
    keymap; CodeDiff still loads on its command.

--- lua/plugins/codediff.lua
+++ lua/plugins/codediff.lua
@@ -1,8 +1,5 @@
 return {
   "esmuellert/codediff.nvim",
   cmd = "CodeDiff",
-  keys = {
-    { "<leader>gc", "<cmd>CodeDiff<cr>", desc = "CodeDiff (changed files)" },
-  },
   opts = {},
 }
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,6 +3,7 @@ return {
   cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
   keys = {
     { "<leader>gD", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+    { "<leader>gp", "<cmd>DiffviewOpen origin/main...HEAD<cr>", desc = "Diffview PR review (vs origin/main)" },
     { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (Diffview)" },
   },
   opts = {},
